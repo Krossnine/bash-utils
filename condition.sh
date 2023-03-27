518ba8e39e74364b50866753a4e8ecfff535506e
Jeremy Wallez <jeremy.wallez@gmail.com>
2023-03-27 11:51:56 +0200

    feat: add condition bash utils

--- condition.sh
+++ condition.sh
@@ -3,10 +3,19 @@ set -e
 
 #
 # myArray=("blah" "anotherString" "ok")
-# Usage : exists_in_array "blah" "${myArray[@]}"
+# Usage : ensure_in_array "blah" "${myArray[@]}"
 #
-function exists_in_array() {
-  local value=$1
-  local array=$2
-  [[ " ${array[*]} " =~ " ${value} " ]] && echo 0 || echo 1
+function ensure_in_array() {
+  local search_value=$1
+  shift
+  local array=("$@")
+  for i in "${array[@]}"
+  do
+      if [ "$i" == "$search_value" ]
+      then
+          return 0
+      fi
+  done
+  echo >&2 "Invalid value $search_value. Should match : ${array[@]}"
+  exit 1
 }
