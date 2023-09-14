bf4b5a94e61296aa996af085d85b6c660afd053a
Krossnine <jeremy.wallez@gmail.com>
2023-09-14 17:16:37 +0200

    feat: add headless warmup bash utils

--- warmup.sh
+++ warmup.sh
@@ -19,3 +19,41 @@ function warmup() {
       fi
   done
 }
+
+function get_sha1_from_url() {
+  local url=$1
+  echo -n $url | sha1sum | awk '{print $1}'
+}
+
+########################################################################################################################
+# Warmup a list of urls using an headless browser
+# Usage :
+# warmup_urls=(https://www.domain.com https://www.another.com)
+# warmup_headless "DESKTOP" "${warmup_urls[@]}" for desktop view
+# warmup_headless "MOBILE" "${warmup_urls[@]}" for mobile view
+########################################################################################################################
+function warmup_headless() {
+  local device=${1:-"DESKTOP"}
+  shift
+  local urls=("$@")
+  for i in "${urls[@]}"; do
+    local URL=$i
+    case $device in
+      "MOBILE") window_size="412,732";;
+      *) window_size="1200,800";;
+    esac
+    docker container run -it -d --rm -v $(pwd)/screenshots:/usr/src/app zenika/alpine-chrome:latest --no-sandbox --screenshot=screenshot.png --hide-scrollbars --window-size=$window_size "$URL"
+
+    while inotifywait --timeout 2 --event create screenshots;
+      do echo "Waiting for file screenshot.png";
+    done
+
+    screenshot_id=$( get_sha1_from_url "$URL")
+
+    if [ -e screenshots/screenshot.png ]
+    then
+      echo "Screenshot was generated"
+      mv screenshots/screenshot.png screenshots/screenshot_device_"$device"_for_"$screenshot_id".png
+    fi
+  done
+}
