f546b2e97f4067a5d8ac144d973c8b89d81b732b
Krossnine <jeremy.wallez@gmail.com>
2023-12-06 12:32:42 +0100

    refacto: bash utils install and add aws_assume_role utils

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-function install() {
-  local INSTALL_PATH=${1:-./}
-  FILENAMES=(
+SCRIPTS_FILENAMES=(
+    "aws_assume_role.sh"
     "aws_tfstate.sh"
     "circleci.sh"
     "condition.sh"
@@ -13,20 +12,47 @@ function install() {
     "github.sh"
     "teams.sh"
     "warmup.sh"
-  )
-  for filename in "${FILENAMES[@]}"; do
-    FILEPATH="$INSTALL_PATH/$filename"
-    source "$FILEPATH"
+)
+REMOTE_INSTALL_PREFIX="https://raw.githubusercontent.com/Krossnine/bash-utils/main"
+CHECK_INSTALL_FUNCTION=circleci_log
+
+function check_install() {
+  fn_exists $CHECK_INSTALL_FUNCTION && echo "Successful installation of bash-utils" || echo "Error on bash-utils install"
+}
+
+function source_remote_script() {
+  local url=$1
+  tmp_file=$(mktemp)
+  http_code=$(curl -s --write-out '%{http_code}' --request "GET" --url "$url" -o "$tmp_file")
+  if ((http_code < 200 || http_code > 299)); then
+    echo "Skip remote install of $url"
+  else
+    # shellcheck disable=SC1090
+    echo "Remote install of $url" && source "$tmp_file"
+  fi
+}
+
+function __install() {
+  local remote_install=${1:-true}
+
+  for filename in "${SCRIPTS_FILENAMES[@]}"; do
+    if [ "$remote_install" = true ] ; then
+      source_remote_script "$REMOTE_INSTALL_PREFIX/$filename"
+    else
+      # shellcheck disable=SC1090
+      echo "Local install from $filename" && source "./$filename"
+    fi
   done
 
-  fn_exists circleci_log && echo "Successful installation of bash-utils" || echo "Error on bash-utils install"
+  check_install
 }
 
-function installFromGit() {
-  INSTALL_PATH="$(mktemp -d)/bash-utils"
-  git clone --quiet "https://github.com/Krossnine/bash-utils.git" "$INSTALL_PATH"
-  install "$INSTALL_PATH"
-  rm -rf "$INSTALL_PATH"
+function local_install() {
+  __install false
 }
 
-installFromGit
+function remote_install() {
+  __install true
+}
+
+remote_install
